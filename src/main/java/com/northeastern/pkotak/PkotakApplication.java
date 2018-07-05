5d7c7db121d61086181386735f99bd1d3e18ca94
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-05 18:05:00 -0400

    Created user admin template

---
+++ src/main/java/com/northeastern/pkotak/PkotakApplication.java
@@ -0,0 +1,12 @@
+package com.northeastern.pkotak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkotakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkotakApplication.class, args);
+    }
+}
