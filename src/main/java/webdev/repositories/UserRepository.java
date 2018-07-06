8f74222b190da80c149b9eab3b7fa4f3e24f93a9
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-06 13:14:34 -0400

    User repository created

---
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {}
