d3ceb6a99d513e78bf783bfa13866f7cf123157e
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-09 13:37:09 -0400

    User profile update functionality modified

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,11 +1,12 @@
 (function () {
     var $fixedUsername, $password, $firstName, $lastName, $role, $dob, $phone, $email;
     var $updateBtn, $logoutBtn;
+    var id;
     var userService = new UserServiceClient();
     $(init);
     
     function init() {
-        $fixedUsername = $('#staticUsername');
+        $fixedUsername = $('#fixedUsername');
         $password = $('#passwordFld');
         $firstName = $('#firstNameFld');
         $lastName = $('#lastNameFld');
@@ -19,10 +20,14 @@
 
         $updateBtn.click(updateProfile);
         $logoutBtn.click(logout);
+
+        id = getIdFromUrl();
+        renderUI(id);
     }
 
     function updateProfile() {
         var user = {
+            id: id,
             username: $fixedUsername.val(),
             password: $password.val(),
             firstName: $firstName.val(),
@@ -33,10 +38,33 @@
             dob: $dob.val()
         }
 
-        userService.updateProfile(user);
+        userService.updateProfile(user)
+            .then($('.alert').show());
     }
 
     function logout() {
         userService.logout();
+    }
+    
+    function getIdFromUrl() {
+        var url = window.location.href;
+        var id = url.slice(url.indexOf('=')+1);
+        return id;
+    }
+
+    function renderUI(id) {
+        userService.findUserById(id)
+            .then(updateFields);
+    }
+
+    function updateFields(user){
+        $fixedUsername.val(user.username);
+        $password.val(user.password);
+        $firstName.val(user.firstName);
+        $lastName.val(user.lastName);
+        $role.val(user.role);
+        $phone.val(user.phone);
+        $email.val(user.email);
+        $dob.val(user.dob);
     }
 })();
--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -1,6 +1,8 @@
 function UserServiceClient() {
     this.login = login;
     this.register = register;
+    this.updateProfile = updateProfile;
+    this.findUserById = findUserById;
 
     function login(username, password) {
         return fetch('http://localhost:8080/api/login',{
@@ -26,5 +28,20 @@ function UserServiceClient() {
                 'content-type': 'application/json'
             }
         }).then(function (response) { return response.json(); });
+    }
+
+    function updateProfile(user){
+        return fetch('http://localhost:8080/api/profile',{
+            method: 'put',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type' : 'application/json'
+            }
+        }).then(function (response) { return response.json(); });
+    }
+
+    function findUserById(id){
+        return fetch('http://localhost:8080/api/user/'+id)
+            .then(function (response) { return response.json(); });
     }
 }
