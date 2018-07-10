cfbe25433d8b1319ef974212cefe65ce932b1f24
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-09 21:30:28 -0400

    Admin adding a user and populating list functionality added

---
+++ src/main/webapp/jquery/components/admin/user-admin.controller.client.js
@@ -0,0 +1,81 @@
+(function () {
+    $(main);
+    var $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld, $emailFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $userRowTemplate, $tbody;
+    var userService = new UserServiceClient();
+
+    function main(){
+        $usernameFld = $('#userameFld');
+        $passwordFld = $('#passwordFld');
+        $firstNameFld = $('#firstNameFld');
+        $lastNameFld = $("#lastNameFld");
+        $roleFld = $('#roleFld');
+        $emailFld = $('#emailFld');
+
+        $tbody = $('.wbdv-tbody');
+        $userRowTemplate = $('.wbdv-template');
+
+        $('#create').click(createUser);
+        //$('#update').click(updateUser);
+
+        findAllUsers();
+    }
+
+    function createUser() {
+
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+        var role = $('#roleFld').val();
+        var email = $('#emailFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            role: role,
+            email: email
+        };
+
+        userService
+            .createUser(user)
+            .then(findAllUsers);
+    }
+
+    function findAllUsers(){
+        userService.findAllUsers()
+            .then(renderUI);
+    }
+
+    function renderUI(users){
+        console.log('Inside render UI'+users);
+        $tbody.empty();
+        for(var i = 0; i < users.length; i++){
+            var currentUser = users[i];
+            var row = $userRowTemplate.clone();
+            console.log(currentUser.username);
+            row.attr('id', currentUser.id);
+
+            row.find('.wbdv-username')
+                .html(currentUser.username);
+
+            row.find('.wbdv-first-name')
+                .html(currentUser.firstName);
+
+            row.find('.wbdv-last-name')
+                .html(currentUser.lastName);
+
+            row.find('.wbdv-role')
+                .html(currentUser.role);
+
+            row.find('.wbdv-email')
+                .html(currentUser.email);
+
+            $tbody.append(row);
+        }
+    }
+
+})();
