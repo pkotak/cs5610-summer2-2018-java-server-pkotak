03a48b6b8acf7c8400132a56329400df172c5376
Paarth Kotak <paarthkotak@gmail.com>
2018-07-25 18:59:32 -0400

    added cross origin

--- src/main/java/com/northeastern/pkotak/webdev/services/WidgetService.java
+++ src/main/java/com/northeastern/pkotak/webdev/services/WidgetService.java
@@ -3,6 +3,7 @@ package com.northeastern.pkotak.webdev.services;
 import com.northeastern.pkotak.webdev.models.Widget;
 import com.northeastern.pkotak.webdev.repositories.WidgetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WidgetService {
 
     @Autowired
