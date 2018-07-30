824eb65431d8133a6286b4b16966426ed5758bc4
Paarth Kotak <kotak.p@husky.neu.edu>
2018-07-29 22:35:14 -0400

    save widgets to topic

--- src/main/java/com/northeastern/pkotak/webdev/services/WidgetService.java
+++ src/main/java/com/northeastern/pkotak/webdev/services/WidgetService.java
@@ -1,12 +1,11 @@
 package com.northeastern.pkotak.webdev.services;
 
+import com.northeastern.pkotak.webdev.models.Topic;
 import com.northeastern.pkotak.webdev.models.Widget;
+import com.northeastern.pkotak.webdev.repositories.TopicRepository;
 import com.northeastern.pkotak.webdev.repositories.WidgetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +17,9 @@ public class WidgetService {
     @Autowired
     WidgetRepository widgetRepository;
 
+    @Autowired
+    TopicRepository topicRepository;
+
     @GetMapping("/api/widget")
     public List<Widget> findAllWidgets(){
         return (List<Widget>) widgetRepository.findAll();
@@ -26,5 +28,40 @@ public class WidgetService {
     @GetMapping("/api/widget/{id}")
     public Optional<Widget> findWidgetById(@PathVariable("id") int widgetId){
         return widgetRepository.findById(widgetId);
+    }
+
+    @GetMapping("/api/topic/{topicId}/widget")
+    public List<Widget> findWidgetsForTopic(@PathVariable("topicId") int topicId){
+        Optional<Topic> optionalTopic = topicRepository.findById(topicId);
+        if(optionalTopic.isPresent()){
+            Topic topic = optionalTopic.get();
+            return topic.getWidgets();
+        }
+        return null;
+    }
+
+    @PostMapping("/api/topic/{topicId}/widget")
+    public List<Widget> saveWidgets(@PathVariable("topicId") int topicId, @RequestBody List<Widget> widgets){
+        Optional<Topic> optionalTopic = topicRepository.findById(topicId);
+        if(optionalTopic.isPresent()){
+            Topic topic = optionalTopic.get();
+            for(Widget w : widgets){
+                w.setTopic(topic);
+                widgetRepository.save(w);
+            }
+            return (List<Widget>) widgetRepository.findAll();
+        }
+        return null;
+    }
+
+    @PutMapping("/api/widget/{widgetId}")
+    public Widget updateWidget(@PathVariable("widgetId") int widgetId,@RequestBody Widget updatedWidget){
+        Optional<Widget> optionalWidget = widgetRepository.findById(widgetId);
+        if(optionalWidget.isPresent()){
+            Widget widget = optionalWidget.get();
+            //TODO: add set Method in widget Model
+            return widget;
+        }
+        return null;
     }
 }
